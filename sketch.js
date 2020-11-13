95dda99ea248ff43c0229e5b4630c9dd8006a177
Rishabhjaiswal2006 <72344228+Rishabhjaiswal2006@users.noreply.github.com>
2020-11-13 20:57:20 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,49 +1,50 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
-var engine, world;
-var box1, pig1;
-var backgroundImg,platform;
+var MYengine, MYworld;
+var box1, pig1,back;
+var platform;
 
-function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+function preload(){
+  back=loadImage("sprites/bg.png");
 }
 
 function setup(){
-    var canvas = createCanvas(1200,400);
-    engine = Engine.create();
-    world = engine.world;
+    createCanvas(1200,400);
+    MYengine = Engine.create();
+    MYworld = MYengine.world;
 
+    
+    ground = new Ground(600,390,1200,20)
+    platform = new Ground(180,300,350,165);
+    box1 = new Box(700,320);
+    box2 = new Box(920,320);
+    pig1 = new Pigs(810, 350);
+    log1 = new Logs(810,260,300, PI/2);
 
-    ground = new Ground(600,height,1200,20);
-    platform = new Ground(200,300,300,200);
+    box3 = new Box(700,240);
+    box4 = new Box(920,240);
+    pig3 = new Pigs(810, 220);
 
-    box1 = new Box(700,320,70,70);
-    box2 = new Box(920,320,70,70);
-    pig1 = new Pig(810, 350);
-    log1 = new Log(810,260,300, PI/2);
+    log3 =  new Logs(810,180,300, PI/2);
 
-    box3 = new Box(700,240,70,70);
-    box4 = new Box(920,240,70,70);
-    pig3 = new Pig(810, 220);
+    box5 = new Box(810,160);
+    log4 = new Logs(760,120,150, PI/7);
+    log5 = new Logs(870,120,150, -PI/7);
 
-    log3 =  new Log(810,180,300, PI/2);
+    bird = new Bird(270,50);
 
-    box5 = new Box(810,160,70,70);
-    log4 = new Log(760,120,150, PI/7);
-    log5 = new Log(870,120,150, -PI/7);
-
-    bird = new Bird(100,100);
-
+    //conlog = new Logs(200,150,100,PI/2);
+    sling= new SlingShot(bird.body,{x:270,y:50});
+   
 }
 
 function draw(){
-    background(backgroundImg);
-    Engine.update(engine);
-    console.log(box2.body.position.x);
-    console.log(box2.body.position.y);
-    console.log(box2.body.angle);
+    background(back);
+    Engine.update(MYengine);
+    
     box1.display();
     box2.display();
     ground.display();
@@ -60,5 +61,17 @@ function draw(){
     log5.display();
 
     bird.display();
-    platform.display();
+    //conlog.display();
+
+   platform.display();
+   sling.display();
+   
+
 }
+function mouseDragged(){
+  Matter.Body.setPosition(bird.body,{x:mouseX,y:mouseY})
+}
+
+function mouseReleased(){
+  sling.fly();
+} 
